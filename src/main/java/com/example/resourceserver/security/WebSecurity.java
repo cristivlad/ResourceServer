f7d2448d6f065fb9c2d5c3ccb7a99d67ea7d867a
cristiii <cristian.vlad10@gmail.com>
2023-02-23 12:52:18 +0200

    added role check

--- src/main/java/com/example/resourceserver/security/WebSecurity.java
+++ src/main/java/com/example/resourceserver/security/WebSecurity.java
@@ -1,20 +1,26 @@
 package com.example.resourceserver.security;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 public class WebSecurity {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpRequest) throws Exception {
+        var jwtAuthConverter = new JwtAuthenticationConverter();
+        jwtAuthConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+
         httpRequest
                 .authorizeHttpRequests()
-                .requestMatchers(HttpMethod.GET, "/users").hasAuthority("SCOPE_profile")
+                .requestMatchers("/users/status/check").hasRole("developer")
                 .anyRequest().authenticated()
                 .and()
-                .oauth2ResourceServer().jwt();
+                .oauth2ResourceServer().jwt()
+                .jwtAuthenticationConverter(jwtAuthConverter);
         return httpRequest.build();
     }
 }
