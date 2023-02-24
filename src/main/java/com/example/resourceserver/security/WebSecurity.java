886f28f23df7a5e741305a35424e7450762877de
cristiii <cristian.vlad10@gmail.com>
2023-02-24 09:54:50 +0200

    use @Secured to secure endpoint for a specific role

--- src/main/java/com/example/resourceserver/security/WebSecurity.java
+++ src/main/java/com/example/resourceserver/security/WebSecurity.java
@@ -2,11 +2,13 @@ package com.example.resourceserver.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity(securedEnabled = true)
 public class WebSecurity {
 
     @Bean
@@ -24,3 +26,6 @@ public class WebSecurity {
         return httpRequest.build();
     }
 }
+
+
+// nu sunt platitor de tva; facturez fara tva acum
